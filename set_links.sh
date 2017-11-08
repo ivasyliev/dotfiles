22c85b2ce2429cfb7d23c4ddaaa74127d18c069d
Ivan Vasyliev <pretor21@gmail.com>
2017-11-08 09:37:42 +0200

    added vundle setup in set_links.sh

--- set_links.sh
+++ set_links.sh
@@ -4,3 +4,7 @@ rm ~/.gitconfig
 rm ~/.vimrc
 ln -s ~/Documents/.gitconfig ~/.gitconfig
 ln -s ~/Documents/.vimrc ~/.vimrc
+
+#load & setup vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
