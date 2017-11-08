0359082fdfd5bf9770a972f314f865a280f41a46
Ivan Vasyliev <pretor21@gmail.com>
2017-11-08 09:45:01 +0200

    fixed link in install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 # script to setup static links for config files in home directory
 rm ~/.gitconfig
 rm ~/.vimrc
-ln -s ~/Documents/.gitconfig ~/.gitconfig
-ln -s ~/Documents/.vimrc ~/.vimrc
+ln -s ~/Documents/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/Documents/dotfiles/.vimrc ~/.vimrc
 
 #load & setup vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
